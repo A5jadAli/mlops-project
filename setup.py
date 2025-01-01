1ce3a3989acedf531aa5d6cb30dad5002a2786d5
Asjad <aliasjid009@gmail.com>
2025-01-01 18:24:23 +0500

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Asjad Ali",
+    author_email="aliasjid009@gmail.com",
+    packages=find_packages()
+)
